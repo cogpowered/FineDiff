d399e3d0d57267a4ba7c21d44e9540f4bdf291e0
Rob Crowe <hello@vivalacrowe.com>
2013-03-11 21:54:28 +0000

    Started tests

---
+++ tests/Delimiters/ConstantsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace FineDiffTests;
+
+use PHPUnit_Framework_TestCase;
+use cogpowered\FineDiff\Delimiters;
+
+class ConstantsTest extends PHPUnit_Framework_TestCase
+{
+    public function testParagraphConstant()
+    {
+        $this->assertEquals(Delimiters::PARAGRAPH, "\n\r");
+    }
+
+    public function testSentenceConstant()
+    {
+        $this->assertEquals(Delimiters::SENTENCE, ".\n\r");
+    }
+
+    public function testWordConstant()
+    {
+        $this->assertEquals(Delimiters::WORD, " \t.\n\r");
+    }
+
+    public function testCharacterConstant()
+    {
+        $this->assertEquals(Delimiters::CHARACTER, "");
+    }
+}
---
+++ tests/Delimiters/EnumTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FineDiffTests;
+
+use PHPUnit_Framework_TestCase;
+
+class EnumTest extends PHPUnit_Framework_TestCase
+{
+    public function testCantInstantiate()
+    {
+        $class   = new \ReflectionClass('cogpowered\FineDiff\Delimiters');
+        $methods = $class->getMethods(\ReflectionMethod::IS_PRIVATE);
+
+        $this->assertTrue(count($methods) >= 1);
+
+        $found = false;
+
+        foreach ($methods as $method) {
+            if ($method->name === '__construct') {
+                $found = true;
+                $this->assertTrue(true);
+                break;
+            }
+        }
+
+        if (!$found) {
+            $this->assertTrue(false);
+        }
+    }
+}
