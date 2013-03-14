95bedf1a40e1278aa2fee3bc916b704ddec94e95
Rob Crowe <hello@vivalacrowe.com>
2013-03-14 17:21:13 +0000

    Aligned opening docblock correctly

--- src/cogpowered/FineDiff/Diff.php
+++ src/cogpowered/FineDiff/Diff.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
-* FINE granularity DIFF
-*
-* Computes a set of instructions to convert the content of
-* one string into another.
-*
-* Originally created by Raymond Hill (https://github.com/gorhill/PHP-FineDiff), brought up
-* to date by Cog Powered (https://github.com/cogpowered/FineDiff).
-*
-* @copyright Copyright 2011 (c) Raymond Hill (http://raymondhill.net/blog/?p=441)
-* @copyright Copyright 2013 (c) Robert Crowe (http://cogpowered.com)
-* @link https://github.com/cogpowered/FineDiff
-* @version 0.0.1
-* @license MIT License (http://www.opensource.org/licenses/mit-license.php)
-*/
+ * FINE granularity DIFF
+ *
+ * Computes a set of instructions to convert the content of
+ * one string into another.
+ *
+ * Originally created by Raymond Hill (https://github.com/gorhill/PHP-FineDiff), brought up
+ * to date by Cog Powered (https://github.com/cogpowered/FineDiff).
+ *
+ * @copyright Copyright 2011 (c) Raymond Hill (http://raymondhill.net/blog/?p=441)
+ * @copyright Copyright 2013 (c) Robert Crowe (http://cogpowered.com)
+ * @link https://github.com/cogpowered/FineDiff
+ * @version 0.0.1
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
 namespace cogpowered\FineDiff;
 
@@ -26,8 +26,8 @@ use cogpowered\FineDiff\Render\Html;
 use cogpowered\FineDiff\Parser\Parser;
 
 /**
-* Diff class.
-*/
+ * Diff class.
+ */
 class Diff
 {
     /**
--- src/cogpowered/FineDiff/Parser/Operations/OperationInterface.php
+++ src/cogpowered/FineDiff/Parser/Operations/OperationInterface.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
-* FINE granularity DIFF
-*
-* Computes a set of instructions to convert the content of
-* one string into another.
-*
-* Originally created by Raymond Hill (github.com/gorhill/PHP-FineDiff), brought up
-* to date by Cog Powered (github.com/cogpowered/FineDiff).
-*
-* @copyright Copyright 2011 (c) Raymond Hill (http://raymondhill.net/blog/?p=441)
-* @copyright Copyright 2013 (c) Robert Crowe (http://cogpowered.com)
-* @link https://github.com/cogpowered/FineDiff
-* @version 0.0.1
-* @license MIT License (http://www.opensource.org/licenses/mit-license.php)
-*/
+ * FINE granularity DIFF
+ *
+ * Computes a set of instructions to convert the content of
+ * one string into another.
+ *
+ * Originally created by Raymond Hill (https://github.com/gorhill/PHP-FineDiff), brought up
+ * to date by Cog Powered (https://github.com/cogpowered/FineDiff).
+ *
+ * @copyright Copyright 2011 (c) Raymond Hill (http://raymondhill.net/blog/?p=441)
+ * @copyright Copyright 2013 (c) Robert Crowe (http://cogpowered.com)
+ * @link https://github.com/cogpowered/FineDiff
+ * @version 0.0.1
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
 namespace cogpowered\FineDiff\Parser\Operations;
 
